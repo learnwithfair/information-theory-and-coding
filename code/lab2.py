f2a59e51fe1ecb16fb9c6b1e1b372c1f658a0c03
learnwithfair <learnwithfair@gmail.com>
2024-03-01 21:52:28 +0600

    Information theory and coding

--- code/lab2.py
+++ code/lab2.py
@@ -9,14 +9,22 @@ def encode(msg, K, n):
             raise ValueError(f'You entered {len(sub_g)} bits.\n need to enter {K} bits')
         g.append(sub_g)
     for i in range(n):
-        res = list(np.poly1d(g[i]) * np.poly1d(msg))
+        res = list(np.poly1d(g[i]) * np.poly1d(msg)) # 1 1 0 0 0 0 1 1 and 1 1 1 0 1 0 0 1
         v.append(res)
 
-    listMax = max(len(l) for l in v)
+    listMax = max(len(l) for l in v) # 1 1 0 0 0 0 1 1 = 8
     for i in range(n):
         if len(v[i]) != listMax:
-            tmp = [0] * (listMax - len(v[i]))
+            tmp = [0] * (listMax - len(v[i])) # Fill Up same len
             v[i] = tmp + v[i]
+    
+    # Print U1(x) and U2(x)
+    print("Answer: ")
+    print("-"*50)
+    result = [[element % 2 for element in sublist] for sublist in v]
+    print("u1(x) : ",result[0])
+    print("u2(x) : ",result[1])
+    
     res = []
     for i in range(listMax):
         res += [v[j][i] % 2 for j in range(n)]
@@ -29,8 +37,8 @@ n = int(input('Number of output(generator): '))
 print('Encoded Message', encode(message, K, n))
 
 # message= 1 0 1 0 1
-# K=4
-# n=2
-# g0= 1 1 1 1
-# g1= 1 1 0 1
+# K=4  = m+1 here, m = no of shift register
+# n=2 n= no of modulo-2 adder
+# g0= 1 1 1 1 Upper
+# g1= 1 1 0 1 Lower
 # Encoded Message: 1 1 1 1 0 1 0 0 0 1 0 0 1 0 1 1
--- code/test.py
+++ code/test.py
@@ -1,8 +1,41 @@
-myDict = {'ravi': 10, 'rajnish': 9,
-		'sanjeev': 15, 'yash': 2, 'suraj': 32}
+import numpy as np
 
-myKeys = list(myDict.keys())
-myKeys.sort()
-sorted_dict = {i: myDict[i] for i in myKeys}
 
-print(sorted_dict)
+def encode(msg, K, n):
+    g, v = [], []
+    for i in range(n):
+        sub_g = list(map(int, input(f'Enter bits for generator {i}: ').split()))
+        if len(sub_g) != K:
+            raise ValueError(f'You entered {len(sub_g)} bits.\n need to enter {K} bits')
+        g.append(sub_g)
+    for i in range(n):
+        res = list(np.poly1d(g[i]) * np.poly1d(msg))
+        print 
+        print("res",res)
+        v.append(res)
+        print(v)
+
+    listMax = max(len(l) for l in v)
+    print("listMax",listMax)
+    for i in range(n):
+        if len(v[i]) != listMax:
+            tmp = [0] * (listMax - len(v[i]))
+            v[i] = tmp + v[i]
+    res = []
+    for i in range(listMax):
+        res += [v[j][i] % 2 for j in range(n)]
+        print("res",res)
+    return res
+
+
+message = list(map(int, "1 0 1 0 1".split()))
+K = 4
+n = 2
+print('Encoded Message', encode(message, K, n))
+
+# message= 1 0 1 0 1
+# K=4  = m+1 here, m = no of shift register
+# n=2 n= no of modulo-2 adder
+# g0= 1 1 1 1 Upper
+# g1= 1 1 0 1 Lower
+# Encoded Message: 1 1 1 1 0 1 0 0 0 1 0 0 1 0 1 1
